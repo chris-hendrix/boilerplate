54d8ab9592d4c0314ae174c0552dc04636f2fed0
Chris Hendrix <chendrix1123@gmail.com>
2022-11-24 12:28:10 -0600

    add theme and reorganize client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,44 @@
 import React from 'react'
+import theme from './theme/theme'
+import { ThemeProvider } from '@mui/material/styles'
 import { Box, Link } from '@mui/material'
-import Counter from './features/counter/Counter'
+import { Counter } from './components'
 
 const App = () => {
   return (
-    <Box className="App">
-      <Box typography="body1">
-        <Counter sx={{ mb: 2 }} />
+    <ThemeProvider theme={theme}>
+      <Box
+        className="App"
+        typography="body1"
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          height: '100vh',
+          bgcolor: 'grey.700'
+        }}
+      >
+        <Box
+          className="layout"
+          sx={{
+            bgcolor: 'grey.300',
+            p: 4,
+          }}
+        >
+          <Counter sx={{ mb: 2 }} />
         Edit <code>src/App.tsx</code> and save to reload.
-        <Box sx={{ mt: 1 }}>
-          {'Learn '}
-          <Link href="https://redux-toolkit.js.org/" target="_blank">
+          <Box sx={{ mt: 1 }}>
+            {'Learn '}
+            <Link href="https://redux-toolkit.js.org/" target="_blank">
             Redux Toolkit
-          </Link>
-          {' and '}
-          <Link href="https://react-redux.js.org/" target="_blank">
-            {'React Redux'}
-          </Link>
+            </Link>
+            {' and '}
+            <Link href="https://react-redux.js.org/" target="_blank">
+              {'React Redux'}
+            </Link>
+          </Box>
         </Box>
       </Box>
-    </Box>
+    </ThemeProvider>
   )
 }
 
