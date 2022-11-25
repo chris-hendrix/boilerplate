2f00c6325c0cff02190dc06ab30b30b8721add1b
Chris Hendrix <chendrix1123@gmail.com>
2022-11-25 08:39:39 -0600

    some more reorg

--- client/src/store/counter.ts
+++ client/src/store/counter.ts
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { RootState, AppThunk } from './store'
+import { RootState, AppThunk } from 'store'
 
 // A mock function to mimic making an async request for data
 const fetchCount = (amount = 1) => {
