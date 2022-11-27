596f5dd6713e97c2dcecff2d37b9601d29d3e28f
Chris Hendrix <chendrix1123@gmail.com>
2022-11-27 16:05:27 -0600

    update docker compose and fe config

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,1 +1,9 @@
-export const baseApiUrl = process.env.REACT_APP_BASE_API_URL
+
+const NODE_ENV = process.env.NODE_ENV || 'dev'
+const REACT_APP_BASE_API_URL = process.env.REACT_APP_BASE_API_URL || 'http://localhost:3001/api'
+const REACT_APP_TEST_BASE_API_URL = process.env.REACT_APP_TEST_BASE_API_URL || 'http://localhost:3010/api'
+
+const baseApiUrl = NODE_ENV === 'test' ? REACT_APP_TEST_BASE_API_URL : REACT_APP_BASE_API_URL
+const nodeEnv = NODE_ENV
+
+export default { baseApiUrl, nodeEnv }
