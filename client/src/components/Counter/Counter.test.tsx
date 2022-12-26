81d3ee103c3d2a98b6841d04af4671e7a9888812
Chris Hendrix <chendrix1123@gmail.com>
2022-12-26 13:12:24 -0600

    remove test util

--- client/src/components/Counter/Counter.test.tsx
+++ client/src/components/Counter/Counter.test.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { expect, renderWithContext, screen, userEvent } from 'utils/test'
 import Counter from './Counter'
+
+import { expect } from '@jest/globals'
+import { Provider } from 'react-redux'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import store from 'store'
+
+const renderWithContext = (element: React.ReactNode) => render(<Provider store={store}>{element}</Provider>)
+export { expect, renderWithContext, render, screen, userEvent }
 
 
 describe('<Counter />', () => {
